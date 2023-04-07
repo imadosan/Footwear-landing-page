1b2a816de480c50786d3b880e70afdc44885a649
Imad Fayad <imad.fayad.if@gmail.com>
2023-04-07 19:30:14 +0200

    add smooth page scrolling

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnOpenModal = document.querySelector('.btn--show-modal');
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#features');
+const nav = document.querySelector('.nav');
+const navLinks = document.querySelector('.nav__links');
+const tabs = document.querySelectorAll('.services__tab');
+const tabsContainer = document.querySelector('.services__tab-container');
+const tabsContent = document.querySelectorAll('services__content');
+
 // Modal Window
 const openModal = (e) => {
   e.preventDefault();
@@ -25,5 +33,19 @@ overlay.addEventListener('click', closeModal);
 document.addEventListener('keydown', (e) => {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
+  }
+});
+
+// Scroll-To Button
+btnScrollTo.addEventListener('click', (e) => {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// Smooth Page Navigation
+navLinks.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
